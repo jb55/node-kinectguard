e7d0e5fa710af967b43038620230404536b14d09
Bill Casarin <bill@casarin.ca>
2011-08-28 01:06:16 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+
+zmq = require('zmq');
+
+cmds = zmq.createSocket('req');
+cmds.connect("tcp://127.0.0.1:5556");
+
+events = zmq.createSocket('sub');
+events.connect("tcp://127.0.0.1:5555");
+events.subscribe(new Buffer(""));
+
+events.on("message", function(msg) {
+  console.log(msg.toString());
+});
+
+cmds.on("message", function(msg){
+  console.log("[CMD]", msg.toString());
+});
+
+cmds.send("get_video_mode");
+cmds.send("get_cutoff");
