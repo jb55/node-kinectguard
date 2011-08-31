b4a23a4a64fc46b7eb43e09dc60fe21faeb44ae0
Bill Casarin <bill@casarin.ca>
2011-08-31 11:52:27 -0700

    Simple bmp test

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ events.connect("tcp://127.0.0.1:5555");
 events.subscribe(new Buffer(""));
 
 events.on("message", function(msg) {
-  console.log(msg.toString());
+  msg = msg.toString();
+  if (msg.indexOf("movement") != -1) {
+    console.log(msg);
+    cmds.send("write_bmp");
+  }
 });
 
 cmds.on("message", function(msg){
@@ -18,3 +22,6 @@ cmds.on("message", function(msg){
 
 cmds.send("get_video_mode");
 cmds.send("get_cutoff");
+cmds.send("set_cutoff 110");
+
+cmds.send("write_bmp");
